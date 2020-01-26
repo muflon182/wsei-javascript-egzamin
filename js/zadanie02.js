c8057257a08b094ad0d23733e2a359170446e727
Mazur Mateusz <mateusz.mazur1@microsoft.wsei.edu.pl>
2020-01-26 15:52:22 +0100

    Zadanie02 egzamin

--- js/zadanie02.js
+++ js/zadanie02.js
@@ -1,6 +1,6 @@
 function concatArray(array1, array2) {
     return array1.concat(array2);
 }
-var array1 = [4,8];
-var array2 = [7,2];
+var array1 = [4, 8];
+var array2 = [7, 2];
 console.log(concatArray(array1, array2));
